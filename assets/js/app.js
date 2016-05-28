338423eb5daeec9f5ee5f3f714731781a9586265
Cecile Nguyen <cecile.ceng@gmail.com>
2016-05-28 14:51:31 -0500

    making pictures work

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,26 +2,31 @@
 var questions = [{
 question: "What was the highest price ever paid for a sushi grade Bluefin Tuna (754 pounds) on January 4th, 2011 at the Tsukiji Fish Market in Tokyo? (In USD)",
 options: ['$25,940', '$396,000', '$1,000,200'],
+image: "assets/images/big-tuna.jpg",
 answer: 1
 },
 {
 question: "What type of fish is saba?",
 options: ['Mackerel', 'Tuna', 'Salmon'],
+image: "assets/images/shimesaba.jpg",
 answer: 0
 },
 {
 question: "In regards to sushi etiquette, which of these is frowned upon?",
 options: ['Eating each piece of sushi in one bite', 'Eating sushi with your fingers', 'Adding a slice of ginger to your fish before eating it'],
+image: "assets/images/sushi_etiquette.jpg",
 answer: 2
 },
 {
 question: "Sake is a Japanese liquor made from ...?",
 options: ['Rice', 'White grapes', 'Green tea and barley'],
+image: "assets/images/sake.jpg",
 answer: 0
 },
 {
 question: "What is the Japanese word for the seaweed used to wrap your roll?",
 options: ['See-weedo', 'Futari', 'Nori'],
+image: "assets/images/nori.jpg",
 answer: 2
 }]
 
@@ -51,11 +56,13 @@ function tryAnswer(answerIndex) {
 	resetTimer();
 	q = questions[currentQuestionIndex];
 	if (q.answer == answerIndex) {
-		alert('Correct!');
+		//alert('Correct!');
+		$('.response').html("Correct!");
 		score++;
-		$('.scoreboard').html(score); 
+		$('.scoreboard').html("Current score: " + score); 
 	} else {
-		alert('Incorrect! The correct answer was ' + q.options[q.answer] + ".") 
+		//alert('Incorrect! The correct answer was ' + q.options[q.answer] + ".") 
+		$('.response').html('Incorrect! The correct answer was ' + q.options[q.answer] + ".");
 	}
 	nextQuestion();
 }
@@ -75,7 +82,10 @@ function askQuestion() {
 	$('.question').html(q.question);
 	for (var i = 0; i < q.options.length; i++) {
 		$('#btn-' + i).html(q.options[i]); 
-		//add in image tag q.image
+		$('#btn-' + i).removeClass('hidden');
+		var img = $('<img>');
+		img.attr('src', q.image);
+		$('.image-container').html(img);
 	}
 	startTimer(20);
 }
@@ -86,6 +96,7 @@ var intervalID = null;
 
 function startTimer(startTime) {
 	timer = startTime;
+	$('.time-left').html('<h2>' + timer + '</h2>');
 	intervalID = setInterval(updateTimer, 1000);
 }
 
